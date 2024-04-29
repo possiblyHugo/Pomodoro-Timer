62d59eb2ec4814abe1d1153666e98afb039c6471
possiblyHugo <alainhandzar@gmail.com>
2024-04-29 16:16:38 -0700

    Play Sound function

--- src/go.mod
+++ src/go.mod
@@ -2,4 +2,12 @@ module pomodoro
 
 go 1.22.2
 
-require github.com/gopxl/beep v1.4.1 // indirect
+require github.com/hajimehoshi/oto v1.0.1
+
+require (
+	github.com/hajimehoshi/go-mp3 v0.3.4
+	golang.org/x/exp v0.0.0-20190306152737-a1d7652674e8 // indirect
+	golang.org/x/image v0.0.0-20190227222117-0694c2d4d067 // indirect
+	golang.org/x/mobile v0.0.0-20190415191353-3e0bab5405d6 // indirect
+	golang.org/x/sys v0.18.0 // indirect
+)
--- src/main.go
+++ src/main.go
@@ -1,10 +1,43 @@
-package pomodoro
+package main
 
 import (
-	"fmt"
-	"github.com/gopxl/beep"
+	"io"
+	"log"
+	"os"
+
+	"github.com/hajimehoshi/go-mp3"
+	"github.com/hajimehoshi/oto"
 )
 
-func main() {
+func PlaySound() error {
+	f, err := os.Open("../audio/ding.mp3")
+	if err != nil {
+		return err
+	}
+	defer f.Close()
 
+	d, err := mp3.NewDecoder(f)
+	if err != nil {
+		return err
+	}
+
+	c, err := oto.NewContext(d.SampleRate(), 2, 2, 8192)
+	if err != nil {
+		return err
+	}
+	defer c.Close()
+
+	p := c.NewPlayer()
+	defer p.Close()
+
+	if _, err := io.Copy(p, d); err != nil {
+		return err
+	}
+	return nil
+}
+
+func main() {
+	if err := PlaySound(); err != nil {
+		log.Fatal(err)
+	}
 }
